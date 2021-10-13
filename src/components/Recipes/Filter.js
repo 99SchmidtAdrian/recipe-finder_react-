4dd813f53917a1d8bc21c7e16b2ff85f05bac3f3
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2021-10-13 19:08:26 +0200

    cleaning the code

--- src/components/Recipes/Filter.js
+++ src/components/Recipes/Filter.js
@@ -8,85 +8,82 @@ const Filter = (props) => {
   const [glutenFreeIsChecked, setGlutenFreeIsChecked] = useState(false);
   const [filterIsOpened, setFilterIsOpened] = useState(false);
 
-  const veganFilter = () => {
-    props.onFilter("vegan");
-    if (veganIsChecked === false) {
-      setVeganIsChecked(true);
-    } else if (veganIsChecked === true) {
-      setVeganIsChecked(false);
-    }
-  };
-  const ketoFilter = () => {
-    props.onFilter("keto");
-    if (ketoIsChecked === false) {
-      setKetoIsChecked(true);
-    } else if (ketoIsChecked === true) {
-      setKetoIsChecked(false);
-    }
-  };
-  const glutenFreeFilter = () => {
-    props.onFilter("gluten-free");
-    if (glutenFreeIsChecked === false) {
-      setGlutenFreeIsChecked(true);
-    } else if (glutenFreeIsChecked === true) {
-      setGlutenFreeIsChecked(false);
-    }
-  };
-  const clearFilters = () => {
-    props.onClear();
-    setVeganIsChecked(false);
-    setKetoIsChecked(false);
-    setGlutenFreeIsChecked(false);
-  };
-  const filterOpenHandler = () => {
-    if (filterIsOpened === false) {
-      setFilterIsOpened(true);
-    } else {
-      setFilterIsOpened(false);
-    }
-  };
-
   return (
-    <div>
+    <div className="h-[50px] ">
       <button
-        className="absolute h-8 w-8 pt-[10px] ml-3 z-10"
-        onClick={filterOpenHandler}
+        className="absolute pt-[10px] ml-3 z-10 flex flex-row hover:-rotate-3 transition-all duration-1000"
+        onClick={() => {
+          if (filterIsOpened === false) {
+            setFilterIsOpened(true);
+          } else {
+            setFilterIsOpened(false);
+          }
+        }}
       >
-        <img src={filterIcon} alt="Filter icon" />
+        <img src={filterIcon} alt="Filter icon" className="h-8 w-8" />
+        <span className={`${filterIsOpened ? "opacity-0" : "opacity-100"} p-1`}>
+          Filter
+        </span>
       </button>
       <div
         className={`${
-          filterIsOpened ? "w-full border opacity-100" : "w-0 border-none opacity-0"
+          filterIsOpened ? "w-full border opacity-100" : "w-0 opacity-0"
         } transition-all duration-1000  overflow-hidden bg-white rounded-xl border-gray-400 shadow-2xl pt-[10px] w-full text-right`}
       >
         <button
-          onClick={clearFilters}
+          onClick={() => {
+            props.onClear();
+            setVeganIsChecked(false);
+            setKetoIsChecked(false);
+            setGlutenFreeIsChecked(false);
+          }}
           className={`${
             filterIsOpened ? "right-3 " : "-left-10"
-          }  py-1 px-2 text-red-600 font-bold border-2 border-red-600 rounded-xl -mt-1 -mb-10`}
+          }  py-1 px-2 text-red-600 font-bold border-2 border-red-600 rounded-xl -mt-1 mr-1 -mb-10`}
         >
           Clear
         </button>
         <form className="text-center text-lg -mt-6 pb-2">
           <ul className="flex flex-row justify-evenly">
             <FilterItem
-              onFilter={veganFilter}
               isChecked={veganIsChecked}
               isOpened={filterIsOpened}
+              onFilter={() => {
+                props.onFilter("vegan");
+                if (veganIsChecked === false) {
+                  setVeganIsChecked(true);
+                } else if (veganIsChecked === true) {
+                  setVeganIsChecked(false);
+                }
+              }}
             >
               Vegan
             </FilterItem>
             <FilterItem
-              onFilter={ketoFilter}
               isChecked={ketoIsChecked}
               isOpened={filterIsOpened}
+              onFilter={() => {
+                props.onFilter("keto");
+                if (ketoIsChecked === false) {
+                  setKetoIsChecked(true);
+                } else if (ketoIsChecked === true) {
+                  setKetoIsChecked(false);
+                }
+              }}
             >
               Keto
             </FilterItem>
             <FilterItem
-              onFilter={glutenFreeFilter}
               isChecked={glutenFreeIsChecked}
               isOpened={filterIsOpened}
+              onFilter={() => {
+                props.onFilter("gluten-free");
+                if (glutenFreeIsChecked === false) {
+                  setGlutenFreeIsChecked(true);
+                } else if (glutenFreeIsChecked === true) {
+                  setGlutenFreeIsChecked(false);
+                }
+              }}
             >
               Gluten-free
             </FilterItem>
