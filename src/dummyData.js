39e0871a9676a8a2456b022eb4436955d3e051a4
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2021-12-10 07:00:17 +0100

    data format change

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,78 +1,199 @@
 //dummy datas
-
-const redLentilPattiesIngredients = [
-  "red lentils",
-  "vegetable broth",
-  "vegetables",
-  "chickpea flour",
-  "chickpea",
-  "flour",
-  "chia seed",
-  "oil",
-];
-const chrispyChickpeasIngredients = [
-  "chickpea",
-  "olive oil",
-  "sea salt",
-  "garlic powder",
-  "smoked paprika",
-  "cumin",
-  "curry",
-  "cayenne pepper",
-];
-const ketoCrackersIngredients = [
-  "non-dairy milk",
-  "sunflower seeds",
-  "nutritional yeast",
-  "seasoning",
-];
-const veganPancakeIngredients = [
-  "flour",
-  "sugar",
-  "baking powder",
-  "salt",
-  "non-dairy milk",
-  "vinegar",
-  "vanillia",
-  "maple syrup",
-];
-
 const dummyData = [
   {
     name: "red lentil patties",
-    ingredients: redLentilPattiesIngredients,
+    ingredients: [
+      { amount: "1kg", type: "red lentils" },
+      { amount: "1kg", type: "vegetable broth" },
+      { amount: "1kg", type: "vegetables" },
+      { amount: "1kg", type: "chickpea flour" },
+      { amount: "1kg", type: "chia seed" },
+      { amount: "1kg", type: "oil" }
+    ],
+    directions: [
+      {
+        step: "1",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "2",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "3",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "4",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "5",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "6",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      }
+    ],
     time: 35,
     diet: ["vegan", "gluten-free"],
     src: "/assets/images/red-lentil-patties.png",
-    isWeeklySuggestion: "40/2021",
-    id: 1,
+    isWeeklySuggestionOn: "2021-11-01",
+    id: 1
   },
   {
     name: "chrispy chickpeas",
-    ingredients: chrispyChickpeasIngredients,
+    ingredients: [
+      { amount: "1kg", type: "red lentils" },
+      { amount: "1kg", type: "vegetable broth" },
+      { amount: "1kg", type: "vegetables" },
+      { amount: "1kg", type: "chickpea flour" },
+      { amount: "1kg", type: "chia seed" },
+      { amount: "1kg", type: "oil" }
+    ],
+    directions: [
+      {
+        step: "1",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "2",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "3",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "4",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "5",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "6",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      }
+    ],
+
     time: 20,
     diet: ["vegan", "gluten-free"],
-    src: "/assets/images/red-lentil-patties.png",
+    src: "/assets/images/chrispy-chickpeas.jpeg",
     id: 2,
-    isWeeklySuggestion: "40/2021",
+    isWeeklySuggestionOn: "2021-11-01"
   },
   {
     name: "keto crackers",
-    ingredients: ketoCrackersIngredients,
+    ingredients: [
+      { amount: "1kg", type: "red lentils" },
+      { amount: "1kg", type: "vegetable broth" },
+      { amount: "1kg", type: "vegetables" },
+      { amount: "1kg", type: "chickpea flour" },
+      { amount: "1kg", type: "chia seed" },
+      { amount: "1kg", type: "oil" }
+    ],
+    directions: [
+      {
+        step: "1",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "2",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "3",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "4",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "5",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "6",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      }
+    ],
+
     time: 20,
     diet: ["keto", "vegan", "paleo"],
-    src: "/assets/images/red-lentil-patties.png",
+    src: "/assets/images/keto-crackers.jpg",
     id: 3,
-    isWeeklySuggestion: "40/2021",
+    isWeeklySuggestionOn: "2021-11-01"
   },
   {
     name: "vegan pancakes",
-    ingredients: veganPancakeIngredients,
+    ingredients: [
+      { amount: "1kg", type: "red lentils" },
+      { amount: "1kg", type: "vegetable broth" },
+      { amount: "1kg", type: "vegetables" },
+      { amount: "1kg", type: "chickpea flour" },
+      { amount: "1kg", type: "chia seed" },
+      { amount: "1kg", type: "oil" }
+    ],
+    directions: [
+      {
+        step: "1",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "2",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "3",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "4",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "5",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      },
+      {
+        step: "6",
+        direction:
+          "Preheat oven to 350 degrees F (175 degrees C). Grease and flour one 9x13 inch cake pan."
+      }
+    ],
+
     time: 40,
     diet: ["vegan"],
-    src: "/assets/images/red-lentil-patties.png",
+    src: "/assets/images/vegan-pancakes.jpg",
     id: 4,
-  },
+  }
 ];
 
 export default dummyData;
