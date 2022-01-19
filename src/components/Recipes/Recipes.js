cf1825e2d893f956116c821d280db9e0eed86342
99SchmidtAdrian <84144084+99SchmidtAdrian@users.noreply.github.com>
2022-01-19 11:33:16 +0100

    fixing wrong link

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import AllRecipes from "./AllRecipes";
 import Suggestion from "./Suggestion";
-import RecipesPage from "./RecipesPage";
+import RecipesPage from "../ui/RecipesPage";
 
 const Recipes = () => {
   return (
-    <RecipesPage>
+    <RecipesPage >
       <Suggestion />
       <AllRecipes />
     </RecipesPage>
